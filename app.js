6aa6c565cefe578fa4921158d0d1b3d9b572256d
Milad Fallah <falahmilad79@gmail.com>
2022-01-28 18:18:31 +0330

    project starts

---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+try {
+    app.listen(3000);
+    console.log("server Connected");
+} catch (err) {
+    console.log(err);
+}
