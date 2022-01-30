81ceccb3db427c86768d9aa735036c50c524b5ea
Milad Fallah <falahmilad79@gmail.com>
2022-01-30 13:54:19 +0330

    database connection added using MongoDB

--- app.js
+++ app.js
@@ -2,16 +2,30 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotEnv = require('dotenv');
+
+const connectDB = require('./config/db');
+
+//Load Config
+dotEnv.config({ path: "./config/config.env"})
+
+//Database Connection
+connectDB();
 
 const app = express();
 
+//BodyParser
 app.use(bodyParser.urlencoded({extended: false}));
 
+//Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+const PORT = process.env.PORT || 3000;
 try {
-    app.listen(3000);
-    console.log("server Connected");
+    app.listen(PORT || 3000);
+    console.log(`server is running on port ${PORT}`);
 } catch (err) {
     console.log(err);
 }
